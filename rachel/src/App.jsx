17d27d5f016c2abff39d8dd58e5962a07235b49e
BagdadiMan <45622053+BagdadiMan@users.noreply.github.com>
2021-05-20 04:12:49 +0300

    feat(Navbar): Implemented (#3)

--- rachel/src/App.jsx
+++ rachel/src/App.jsx
@@ -1,30 +1,7 @@
 import React from 'react';
-import { UserTimeline } from './Timeline/UserTimeline';
+import { Navbar } from './Navbar/Navbar';
 
 export const App = () => {
-    const text = 'רש"צ'
 
-    const timelineItems = [
-        {
-            id: 1,
-            startDate: new Date(),
-            title: 'מפקד בקורס תכנות',
-            labels: [
-                {
-                    id: 1,
-                    text: 'בסמ"ח'
-                },
-                {
-                    id: 2,
-                    text: 'תכניתן'
-                },
-                {
-                    id: 3,
-                    text: 'Data Analyst'
-                }
-            ]
-        }
-    ]
-
-    return ( <UserTimeline timelineItems={timelineItems}/>);
+    return ( <Navbar  />);
     };
